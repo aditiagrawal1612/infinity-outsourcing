9dec5d9b75475d2ef3cde9dec8a377c222c7448a
aditiagrawal1612 <aditiagrawal0016@gmail.com>
2023-02-18 00:28:43 +0530

    Fixed google form resposiveness

--- src/containers/Registeration.js
+++ src/containers/Registeration.js
@@ -14,9 +14,7 @@ const StyledSection = styled.section`
 `;
 
 const GoogleForm = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  display: table;
+  margin: 0 auto;
 `;
 
 export default function Registeration() {
@@ -26,8 +24,13 @@ export default function Registeration() {
       <GoogleForm>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLScFaKPaszltKi618KcSylVLTAjj2bfaQdq8cS1h-1AUYl0xQQ/viewform?embedded=true"
-          width="640"
-          height="1334"
+          width="100%"
+          style={{
+            top: "0",
+            left: "0",
+            minHeight: "60vh",
+          }}
+          height="100%"
           frameborder="0"
           marginheight="0"
           marginwidth="0"
